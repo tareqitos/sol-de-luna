8cb35bb7df7b71e51e06880858379531c7f66ed8
Tareqitos <contact@tareqitos.com>
2025-05-22 17:22:48 +0200

    Fix emoji being transparent in dark mode

--- components/Flights/CardPassengers.jsx
+++ components/Flights/CardPassengers.jsx
@@ -1,5 +1,5 @@
 
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import { Avatar, Surface, useTheme } from "react-native-paper";
 import Txt from "../Utils/Txt";
 import { generateRandomEmoji } from "../../services/services";
@@ -21,9 +21,9 @@ export default function CardPassengers({ destination, item, passengers }) {
         <View style={{ gap: 10 }}>
             {passengers && passengers.map((passenger) => (
                 <Surface style={[s.surface, { backgroundColor: colors.surface }]} elevation={1} key={passenger.id}>
-                    <TouchableOpacity activeOpacity={.8} onPress={() => updateEmoji(passenger.id)}>
-                        <Avatar.Text label={passenger.emoji} size={36} />
-                    </TouchableOpacity>
+                    <TouchableWithoutFeedback activeOpacity={.8} onPress={() => updateEmoji(passenger.id)}>
+                        <Avatar.Text color="black" label={passenger.emoji} size={36} />
+                    </TouchableWithoutFeedback>
                     {passenger.name && <Txt style={[typography.h5, { flex: 1 }]}>{passenger.name}</Txt>}
                     {passenger.seat && <Txt style={[typography.caption]}>{passenger.seat}</Txt>}
                 </Surface>
