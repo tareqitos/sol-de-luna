b1c3afffac046f97bdf273277d2012a0027326a4
Tareqitos <contact@tareqitos.com>
2025-05-23 18:03:31 +0200

    Create CardDuration.jsx

---
+++ components/Cards/CardDuration.jsx
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import { s } from "../../styles/card.style";
+import { Icon, useTheme } from "react-native-paper";
+import Txt from "../Utils/Txt";
+import { calculateDuration } from "../../services/date-service";
+
+export default function CardDuration({ icon, departureTime, arrivalTime, hasIcon = true }) {
+    const { colors, typography } = useTheme()
+    const duration = calculateDuration(departureTime, arrivalTime)
+
+    return (
+        <View style={s.card.date}>
+            {hasIcon && <Icon source={icon || "clock-outline"} color={colors.primary} size={16} />}
+            <Txt style={[s.card.date, typography.caption, { color: colors.onBackground }]}>{duration.hours + "h" + duration.minutes + "m"}</Txt>
+        </View>
+    )
+}
